38d76ed783ad0f3b473622dbbfeee9fcacf24d74
Priscilla <Priscillayg23@gmail.com>
2017-10-28 14:32:00 -0500

    created chart

--- gauge.js
+++ gauge.js
@@ -37,3 +37,45 @@ bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
 bar.text.style.fontSize = '2rem';
 
 bar.animate(0.7);  // Number from 0.0 to 1.0
+
+
+//Chart.js graph feature
+var Chart = require('chart.js');
+var ctx = "plantChart";
+
+var plantChart = new Chart(ctx, {
+type: 'interpolation',
+data: {
+  labels: ["Oct 27", "Oct 28", "Oct 29"],
+  datasets: [{
+    label: 'Date',
+    data: [5, 25, 15],
+    backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+  }]
+},
+options: {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true
+      }
+    }]
+  }
+}
+});
