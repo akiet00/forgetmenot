fea966d7fd14867a077dd68443ce928ee8c42c84
akiet00 <akiet00@yahoo.com>
2017-10-28 11:27:16 -0500

    init_commit

---
+++ gauge.js
@@ -0,0 +1,39 @@
+
+console.log("Hello I'm in GAGE");
+
+var my_container = document.getElementById('container');
+
+//TESTING PROGRESSBAR LIBRARY
+var bar = new ProgressBar.Circle( my_container, {
+  color: '#aaa',
+  // This has to be the same size as the maximum width to
+  // prevent clipping
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: false
+  },
+  from: { color: '#aaa', width: 1 },
+  to: { color: '#333', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    var value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+
+
+bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontSize = '2rem';
+
+bar.animate(0.7);  // Number from 0.0 to 1.0
